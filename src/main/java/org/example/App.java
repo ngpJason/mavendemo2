8f74be09b5242485de3c32c9047811e790853eb9
lihaiyue <ngpjason@hotmail.com>
2022-07-02 16:57:01 +0800

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*1*/
+        System.out.println( "Hello World!" );
+    }
+}
